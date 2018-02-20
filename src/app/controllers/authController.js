323b89764f49d100bdcc8da49beec28acb5de45f
Rafael França <rafaelfms@gmail.com>
2018-02-20 17:22:13 -0300

    forgot password added

--- src/app/controllers/authController.js
+++ src/app/controllers/authController.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
+const crypto = require('crypto');
+const mailer = require('../../modules/mailer')
 
 const authConfig = require('../../config/auth.json');
 const User = require('../models/User');
@@ -54,6 +56,45 @@ router.post('/authenticate', async (req, res) => {
     token: generateToken({ id: user._id }),
   });
 
+});
+
+router.post('/forgot_password', async (req, res) => {
+  const { email } = req.body;
+
+  try {
+
+    const user = await User.findOne({ email });
+
+    if(!user)
+      return res.status(400).send({ error: 'Usuário não encontrado' });
+    
+    const token = crypto.randomBytes(20).toString('hex');
+    
+    const now = new Date();
+    now.setHours(now.getHours() + 1);
+
+    await User.findByIdAndUpdate(user.id, {
+      '$set': {
+        passwordResetToken: token,
+        passwordResetExpires: now,
+      }
+    });
+
+    mailer.sendMail({
+      to: email,
+      from: 'rafaelfms@gmail.com',
+      template: 'auth/forgot_password',
+      context: { token },
+    }, (err) => {
+      if (err)
+        return res.status(400).send({ error: 'Não foi possível enviar o email, tente novamente' });
+    });
+
+    return res.status(200).send('Email enviado.')
+    
+  } catch (err) {
+    res.status(400).send({ error: 'Erro em esqueci minha senha, tente novamente' });
+  }
 });
 
 module.exports = app => app.use('/auth', router);
