bcf14b22df25fa323acfd93ae6a809148bf8c159
Rafael França <rafaelfms@gmail.com>
2018-02-22 10:53:45 -0300

    init test; code coverage test; ci.

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const mongoose = require('mongoose');
-let database = 'educachat'; // TODO: verificar criação de banco com nome variável
+let database = require('../config/database.json'); // TODO: verificar criação de banco com nome variável
 
-mongoose.connect(`mongodb://localhost/${database}`);
+mongoose.connect(`mongodb://localhost/${database.name}`);
 mongoose.Promise = global.Promise;
 
 module.exports = mongoose;
